4e8d8bc647e07b0af0579df925e31c0d452a7b07
Angela Norris <cocoanorris@yahoo.com>
2018-09-15 12:35:18 -0400

    reconfigured app to get a front end on it

--- models/article.js
+++ models/article.js
@@ -1,29 +1,33 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require("mongoose");
 
-//create article Schema & model
-const CommentSchema = new Schema({
-  author: {
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
     type: String,
-    required:[true, "Name field is required"],
-    date : Date
+    required: true
   },
-  comment: {
+  // `link` is required and of type String
+  link: {
     type: String,
-    required:[true]
-  }
-
-});
-
-
-
-const ArticleSchema = new Schema({
-  headline: String,
+    required: true
+  },
   summary: String,
-  url: String,
-  comment: [CommentSchema]
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
-const Article = mongoose.model('article', ArticleSchema);
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
 
+// Export the Article model
 module.exports = Article;
